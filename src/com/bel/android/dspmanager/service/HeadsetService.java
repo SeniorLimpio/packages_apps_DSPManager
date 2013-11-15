de074b7498f000edbca05ae999b5ab028b421715
xplodwild <me@xplod.fr>
2013-11-15 12:57:06 +0100

    [1/3] Effects: Add a stereo widening effect
    Based on self knowledge.
    Aside from the filter, a HighPass method has been
    added to Biquad in the DSP library
    
    Change-Id: I5dc8afa75cdf179c71ec38359887be9a5a60d4bd
    
    Conflicts:
    	src/com/bel/android/dspmanager/service/HeadsetService.java
    
    Conflicts:
    	src/com/bel/android/dspmanager/service/HeadsetService.java

--- src/com/bel/android/dspmanager/service/HeadsetService.java
+++ src/com/bel/android/dspmanager/service/HeadsetService.java
@@ -1,7 +1,14 @@
 package com.bel.android.dspmanager.service;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import android.app.Service;
-import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,15 +19,13 @@ import android.media.audiofx.AudioEffect;
 import android.media.audiofx.BassBoost;
 import android.media.audiofx.Equalizer;
 import android.media.audiofx.Virtualizer;
+import android.media.audiofx.StereoWide;
+import android.provider.Settings;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.bel.android.dspmanager.activity.DSPManager;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * <p>This calls listen to events that affect DSP function and responds to them.</p>
@@ -33,140 +38,207 @@ import java.util.UUID;
  * @author alankila
  */
 public class HeadsetService extends Service {
-    /**
-     * Helper class representing the full complement of effects attached to one
-     * audio session.
-     *
-     * @author alankila
-     */
-    protected static class EffectSet {
-        private static final UUID EFFECT_TYPE_VOLUME =
-                UUID.fromString("09e8ede0-ddde-11db-b4f6-0002a5d5c51b");
+	/**
+	 * Helper class representing the full complement of effects attached to one
+	 * audio session.
+	 *
+	 * @author alankila
+	 */
+	protected static class EffectSet {
+		private static final UUID EFFECT_TYPE_VOLUME = UUID
+				.fromString("09e8ede0-ddde-11db-b4f6-0002a5d5c51b");
+		private static final UUID EFFECT_TYPE_NULL = UUID
+				.fromString("ec7178ec-e5e1-4432-a3f4-4657e6795210");
 
-        /** Session-specific dynamic range compressor */
-        public final AudioEffect mCompression;
-        /** Session-specific equalizer */
-        private final Equalizer mEqualizer;
-        /** Session-specific bassboost */
-        private final BassBoost mBassBoost;
-        /** Session-specific virtualizer */
-        private final Virtualizer mVirtualizer;
+		/** Session-specific dynamic range compressor */
+		public final AudioEffect mCompression;
+		/** Session-specific equalizer */
+		private final Equalizer mEqualizer;
+		/** Session-specific bassboost */
+		private final BassBoost mBassBoost;
+		/** Session-specific virtualizer */
+		private final Virtualizer mVirtualizer;
+		/** Session-specific stereo widener */
+		private final StereoWide mStereoWide;
 
-        protected EffectSet(int sessionId) {
-            try {
-                mCompression = new AudioEffect(EFFECT_TYPE_VOLUME,
-                        AudioEffect.EFFECT_TYPE_NULL, 0, sessionId);
-            } catch (IllegalArgumentException e) {
-                throw new RuntimeException(e);
-            } catch (UnsupportedOperationException e) {
-                throw new RuntimeException(e);
-            }
+		protected EffectSet(int sessionId) {
+			try {
+				/*
+				 * AudioEffect constructor is not part of SDK. We use reflection
+				 * to access it.
+				 */
+				mCompression = AudioEffect.class.getConstructor(UUID.class,
+						UUID.class, Integer.TYPE, Integer.TYPE).newInstance(
+						EFFECT_TYPE_VOLUME, EFFECT_TYPE_NULL, 0, sessionId);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			mEqualizer = new Equalizer(0, sessionId);
+			mBassBoost = new BassBoost(0, sessionId);
+			mVirtualizer = new Virtualizer(0, sessionId);
+			mStereoWide = new StereoWide(0, sessionId);
+		}
 
-            mEqualizer = new Equalizer(0, sessionId);
-            mBassBoost = new BassBoost(0, sessionId);
-            mVirtualizer = new Virtualizer(0, sessionId);
-        }
+		protected void release() {
+			mCompression.release();
+			mEqualizer.release();
+			mBassBoost.release();
+			mVirtualizer.release();
+			mStereoWide.release();
+		}
 
-        protected void release() {
-            mCompression.release();
-            mEqualizer.release();
-            mBassBoost.release();
-            mVirtualizer.release();
-        }
-    }
+		/**
+		 * Proxies call to AudioEffect.setParameter(byte[], byte[]) which is
+		 * available via reflection.
+		 *
+		 * @param audioEffect
+		 * @param parameter
+		 * @param value
+		 */
+		private static void setParameter(AudioEffect audioEffect, int parameter, short value) {
+			try {
+				byte[] arguments = new byte[] {
+						(byte) (parameter), (byte) (parameter >> 8),
+						(byte) (parameter >> 16), (byte) (parameter >> 24)
+				};
+				byte[] result = new byte[] {
+						(byte) (value), (byte) (value >> 8)
+				};
 
-    protected static final String TAG = HeadsetService.class.getSimpleName();
+				Method setParameter = AudioEffect.class.getMethod(
+						"setParameter", byte[].class, byte[].class);
+				int returnValue = (Integer) setParameter.invoke(audioEffect,
+						arguments, result);
 
-    public class LocalBinder extends Binder {
-        public HeadsetService getService() {
-            return HeadsetService.this;
-        }
-    }
+				if (returnValue != 0) {
+					Log.e(TAG,
+							String.format(
+									"Invalid argument error in setParameter(%d, (short) %d) == %d",
+									parameter, value, returnValue));
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    private final LocalBinder mBinder = new LocalBinder();
+	protected static final String TAG = HeadsetService.class.getSimpleName();
 
-    /** Known audio sessions and their associated audioeffect suites. */
-    protected final Map<Integer, EffectSet> mAudioSessions = new HashMap<Integer, EffectSet>();
+	public class LocalBinder extends Binder {
+		public HeadsetService getService() {
+			return HeadsetService.this;
+		}
+	}
 
-    /** Is a wired headset plugged in? */
-    protected boolean mUseHeadset;
+	private final LocalBinder mBinder = new LocalBinder();
 
-    /** Is bluetooth headset plugged in? */
-    protected boolean mUseBluetooth;
+	/** Known audio sessions and their associated audioeffect suites. */
+	protected final Map<Integer, EffectSet> mAudioSessions = new HashMap<Integer, EffectSet>();
 
-    /** Is a dock or USB audio device plugged in? */
-    protected boolean mUseUSB;
+	/** Is a wired headset plugged in? */
+	protected boolean mUseHeadset;
 
-    /** Has DSPManager assumed control of equalizer levels? */
-    private float[] mOverriddenEqualizerLevels;
+	/** Is bluetooth headset plugged in? */
+	protected boolean mUseBluetooth;
 
-    /**
-     * Receive new broadcast intents for adding DSP to session
-     */
+	/** Is a dock or USB audio device plugged in? */
+	protected boolean mUseUSB;
+
+	/** Has DSPManager assumed control of equalizer levels? */
+	private float[] mOverriddenEqualizerLevels;
+
+	/**
+	 * Receive new broadcast intents for adding DSP to session
+	 */
     private final BroadcastReceiver mAudioSessionReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            int sessionId = intent.getIntExtra(AudioEffect.EXTRA_AUDIO_SESSION, 0);
-            if (action.equals(AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION)) {
-                Log.i(TAG, String.format("New audio session: %d", sessionId));
-                if (!mAudioSessions.containsKey(sessionId)) {
-                    mAudioSessions.put(sessionId, new EffectSet(sessionId));
-                }
-            }
-            if (action.equals(AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION)) {
-                Log.i(TAG, String.format("Audio session removed: %d", sessionId));
-                EffectSet gone = mAudioSessions.remove(sessionId);
-                if (gone != null) {
-                    gone.release();
-                }
-            }
-            updateDsp();
-        }
-    };
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			int sessionId = intent.getIntExtra(AudioEffect.EXTRA_AUDIO_SESSION, 0);
+			if (action.equals(AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION)) {
+				Log.i(TAG, String.format("New audio session: %d", sessionId));
+				if (! mAudioSessions.containsKey(sessionId)) {
+					mAudioSessions.put(sessionId, new EffectSet(sessionId));
+				}
+			}
+			if (action.equals(AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION)) {
+				Log.i(TAG, String.format("Audio session removed: %d", sessionId));
+				EffectSet gone = mAudioSessions.remove(sessionId);
+				if (gone != null) {
+					gone.release();
+				}
+			}
+			updateDsp();
+		}
+	};
 
-    /**
-     * Update audio parameters when preferences have been updated.
-     */
-    private final BroadcastReceiver mPreferenceUpdateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.i(TAG, "Preferences updated.");
-            updateDsp();
-        }
-    };
+	/**
+	 * Update audio parameters when preferences have been updated.
+	 */
+	private final BroadcastReceiver mPreferenceUpdateReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.i(TAG, "Preferences updated.");
+			updateDsp();
+		}
+	};
 
-    /**
-     * This code listens for changes in bluetooth and headset events. It is
-     * adapted from google's own MusicFX application, so it's presumably the
-     * most correct design there is for this problem.
-     */
-    private final BroadcastReceiver mRoutingReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(final Context context, final Intent intent) {
-            final String action = intent.getAction();
-            final boolean prevUseHeadset = mUseHeadset;
-            final boolean prevUseBluetooth = mUseBluetooth;
-            final boolean prevUseUSB = mUseUSB;
+	/**
+	 * This code listens for changes in bluetooth and headset events. It is
+	 * adapted from google's own MusicFX application, so it's presumably the
+	 * most correct design there is for this problem.
+	 */
+	private final BroadcastReceiver mRoutingReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(final Context context, final Intent intent) {
+			final String action = intent.getAction();
+			final boolean prevUseHeadset = mUseHeadset;
+			final boolean prevUseBluetooth = mUseBluetooth;
+			final boolean prevUseUSB = mUseUSB;
+			final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+			if (action.equals(Intent.ACTION_HEADSET_PLUG)) {
+				mUseHeadset = intent.getIntExtra("state", 0) == 1;
+				boolean launchPlayer = Settings.System.getInt(getContentResolver(),
+					Settings.System.HEADSET_CONNECT_PLAYER, 0) != 0;
+				if (mUseHeadset && launchPlayer) {
+					Intent playerIntent = new Intent(Intent.ACTION_MAIN);
+					playerIntent.addCategory(Intent.CATEGORY_APP_MUSIC);
+					playerIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					startActivity(playerIntent);
+				}
+			} else if (action.equals(BluetoothDevice.ACTION_ACL_CONNECTED)) {
+				final int deviceClass = ((BluetoothDevice) intent
+						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)).getBluetoothClass()
+						.getDeviceClass();
+				if ((deviceClass == BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES)
+						|| (deviceClass == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET)) {
+					mUseBluetooth = true;
+				}
+			} else if (action.equals(Intent.ACTION_ANALOG_AUDIO_DOCK_PLUG)) {
+			    mUseUSB = intent.getIntExtra("state", 0) == 1;
+			} else if (action.equals(AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
+				mUseBluetooth = audioManager.isBluetoothA2dpOn();
+				mUseHeadset = audioManager.isWiredHeadsetOn();
+			} else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+				final BluetoothDevice device =
+						((BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+				if (device == null || device.getBluetoothClass() == null) {
+					return;
+				}
+				final int deviceClass = device.getBluetoothClass().getDeviceClass();
+				if ((deviceClass == BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES)
+						|| (deviceClass == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET)) {
+					mUseBluetooth = false;
+				}
+			}
 
-            if (action.equals(Intent.ACTION_HEADSET_PLUG)) {
-                mUseHeadset = intent.getIntExtra("state", 0) == 1;
-            } else if (action.equals(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED)) {
-                int state = intent.getIntExtra(BluetoothA2dp.EXTRA_STATE,
-                        BluetoothA2dp.STATE_DISCONNECTED);
-                mUseBluetooth = state == BluetoothA2dp.STATE_CONNECTED;
-            } else if (action.equals(Intent.ACTION_ANALOG_AUDIO_DOCK_PLUG) ||
-                       action.equals(Intent.ACTION_DIGITAL_AUDIO_DOCK_PLUG)) {
-                mUseUSB = intent.getIntExtra("state", 0) == 1;
-            }
-
-            Log.i(TAG, "Headset=" + mUseHeadset + "; Bluetooth="
-                    + mUseBluetooth + " ; USB=" + mUseUSB);
-            if (prevUseHeadset != mUseHeadset
-                    || prevUseBluetooth != mUseBluetooth
-                    || prevUseUSB != mUseUSB) {
-                updateDsp();
-            }
+            Log.i(TAG, "Headset=" + mUseHeadset + "; Bluetooth=" + mUseBluetooth +
+                       " ; USB=" + mUseUSB);
+			if (prevUseHeadset != mUseHeadset
+					|| prevUseBluetooth != mUseBluetooth
+					|| prevUseUSB != mUseUSB) {
+				updateDsp();
+			}
         }
     };
 
@@ -317,7 +389,12 @@ public class HeadsetService extends Service {
             session.mVirtualizer.setStrength(
                     Short.valueOf(prefs.getString("dsp.headphone.mode", "0")));
         } catch (Exception e) {
-            Log.e(TAG, "Error enabling virtualizer!");
+            Log.e(TAG, "Error enabling virtualizer!", e);
+	}
+            session.mStereoWide.setEnabled(preferences.getBoolean("dsp.stereowide.enable", false));
+	    session.mStereoWide.setStrength(Short.valueOf(preferences.getString("dsp.stereowide.mode", "0")));
+       } catch (Exception e) {
+            Log.e(TAG, "Error enabling stereo wide!", e);
         }
     }
 }
